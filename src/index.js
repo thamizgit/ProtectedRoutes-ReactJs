7b2dd10cfce1cf9e1bac1b9c8d6c9c6bbceb8e9a
thamizgit <tothamizs4@gmail.com>
2023-06-08 19:39:59 +0530

    updated routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './Context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
